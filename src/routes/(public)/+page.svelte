0490da272449c5e65768bf11b6610f241945c65f
Navindu <navanavi3@gmail.com>
2022-11-09 00:24:35 +0530

    feat: google auth

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -1,22 +1,3 @@
-<div class="text-2xl">Home</div>
-
-<div class="text-xl">Public Routes</div>
-
-<ul>
-	<li><a href="/">Home</a></li>
-	<li><a href="/login">Login</a></li>
-	<li><a href="/sign-up">Sign up</a></li>
-</ul>
-
-<div class="text-xl">Protected Routes</div>
-
-<ul>
-	<li><a href="/profile">Profile</a></li>
-	<li><a href="/catpics">Cat pics</a></li>
-</ul>
-
-<style lang="postcss">
-	a {
-		@apply text-blue-500;
-	}
-</style>
+<div class="w-full h-screen flex text-2xl items-center justify-center">
+	You're not supposed to be here
+</div>
--- src/routes/(public)/login/+page.svelte
+++ src/routes/(public)/login/+page.svelte
@@ -6,17 +6,17 @@
 	let email: string;
 	let password: string;
 
+	// TODO: @Nav
 	const handleLogin = async () => {
 		try {
 			loading = true;
-			const { data, error } = await supabaseClient.auth.signInWithPassword({ email, password });
+			const { data, error } = await supabaseClient.auth.signInWithOAuth({
+				provider: 'google'
+			});
 
 			console.log(data, error);
 
 			if (error) throw error;
-
-			alert('Logged in');
-			goto(`/profile`);
 		} catch (error) {
 			if (error instanceof Error) {
 				alert(error.message);
@@ -30,11 +30,6 @@
 <form class="row flex-center flex" on:submit|preventDefault={handleLogin}>
 	<div class="col-6 form-widget">
 		<h1 class="header">Log in</h1>
-
-		<div>
-			<input class="border p-2" type="email" placeholder="Your email" bind:value={email} />
-			<input class="border p-2" type="password" placeholder="Password" bind:value={password} />
-		</div>
 
 		<div>
 			<input
