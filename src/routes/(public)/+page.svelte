8d6e6d87982c615d257496dd7c9835242135f704
Navindu <navanavi3@gmail.com>
2022-12-01 23:04:51 +0530

    logo

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -1,5 +1,8 @@
 <div class="w-full h-screen flex flex-col gap-4 text-2xl items-center justify-center">
-	<div>You're not supposed to be here</div>
+	<div class="flex items-start justify-center gap-3 text-5xl">
+		<img class="w-14" src="/logo.png" alt="logo" />
+		Kotaa
+	</div>
 
 	<div><a href="/dash" class="text-sm underline">Dashboard</a></div>
 </div>
